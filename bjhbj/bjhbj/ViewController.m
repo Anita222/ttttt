851ef5e0e74a282513d17b1c65e01ada197f3c1d
lk <2398465494@qq.com>
2016-05-04 19:36:07 +0800

    jhj

---
+++ bjhbj/bjhbj/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  bjhbj
+//
+//  Created by lk on 16/5/4.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hahaha");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
